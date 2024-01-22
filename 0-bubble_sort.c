3885bc5d66e03e9f29d6660f4041fb756f663bd9
lupamo <arnoldlupamo@gmail.com>
2024-01-22 08:23:52 +0300

    bubble sort but  not optimized

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,5 +10,21 @@
 
 void bubble_sort(int *array, size_t size)
 {
+	unsigned int i = 0;
+	unsigned int j = 0;
+	unsigned int temp;
 
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
 }
