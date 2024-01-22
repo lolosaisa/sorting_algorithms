c7799c434235bf5f6f6c96a0f861ce02104c92e2
lupamo <arnoldlupamo@gmail.com>
2024-01-22 13:14:51 +0300

    betty

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -22,9 +22,9 @@ void bubble_sort(int *array, size_t size)
 		return;
 	}
 
-	do
-	{
+	do {
 		bubbly = false;
+
 		for (i = 0; i < size - 1; i++)
 		{
 			for (j = 0; j < size - i - 1; j++)
@@ -39,6 +39,6 @@ void bubble_sort(int *array, size_t size)
 				}
 			}
 		}
-		size --;
+		size--;
 	} while (bubbly);
 }
