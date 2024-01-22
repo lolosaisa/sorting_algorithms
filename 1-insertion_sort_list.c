0abf613ac3f2cea97a7b749cc70456c5e9ba8a9d
lupamo <arnoldlupamo@gmail.com>
2024-01-22 22:27:10 +0300

    insertion start

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - using insertion sort algorithm to sort
+ * a list
+ * @list: list to be sorted
+ * Return: Nothing
+*/
+
+void insertion_sort_list(listint_t **list)
+{
+	unsigned int i, j, temp;
+
+}
--- sort.h
+++ sort.h
@@ -35,5 +35,6 @@ typedef enum bool
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
 
 #endif
