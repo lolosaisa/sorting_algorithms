0481e9747f362cbf3ff89084ac68d3d817d714f0
lupamo <arnoldlupamo@gmail.com>
2024-01-22 11:54:28 +0300

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,6 +14,11 @@ void bubble_sort(int *array, size_t size)
 	unsigned int j = 0;
 	unsigned int temp;
 
+	if (array == NULL || size < 2)
+	{
+		return;
+	}
+
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
